140bafc1fdbfa520e9615312b6492e1be803be33
Tyler Bikaun <15507130@student.curtin.edu.au>
2019-11-24 19:24:08 +0800

    Basic functionality for procedural model working.

--- state_model.py
+++ state_model.py
@@ -19,7 +19,7 @@ import matplotlib.pyplot as plt
 
 # Phase 1 development
 
-income = 75000
+income = 50000
 
 start_date = datetime.date(2020,1,1)    # Y-d-m
 current_age = 26
@@ -57,36 +57,74 @@ def tax_model(annual_income):
     return monthly_income_tax
 
 
-def super_model(annual_income, monthly_super_contrib, initial_super_bal, cumulative_super_bal):
+def super_model(model_phase, annual_income, monthly_super_contrib, initial_super_bal, cumulative_super_bal, cumulative_super_val):
+    """
+    Assumes super annuation is run via an institution and is not self managed
     """
     
-    """  
+    # TODO CONVERT ALL INFORMATION TO TODAYS DOLLARS AS PER THE ASSUMPTIONS ON: https://www.moneysmart.gov.au/tools-and-resources/calculators-and-apps/superannuation-calculator
     
     # Monthly Contribution from Employer.
-    contrib_rate = 0.095    # % p.a.
-    monthly_super_contrib = (annual_income * contrib_rate) / 12
+    if model_phase == 1:
+        # Earned Income is active and employer is contributing
+        contrib_rate = 0.095    # % p.a.
+        monthly_super_contrib = (annual_income * contrib_rate) / 12
     
-    # Deductions - Insurance, Administration fees, etc.
-    
-    
-    
-    # Cumulative balance
-    if cumulative_super_bal == 0:
-        # Adds initial balance to monthly super balance
-        cumulative_super_bal += monthly_super_contrib + initial_super_bal
+    # Deductions - Insurance, Administration and investment fees, etc.
+    admin_fee = (100 / 12) # $100 p.a. administration fee
+    investment_fee = (0.01 / 12) * cumulative_super_bal # 1% investment fee p.a.
+    if model_phase == 1:
+        contrib_fee = (0.01 / 12) * cumulative_super_bal # 1% contribution fee p.a.
     else:
-        # if initial balance is added, just add new contributions
-        cumulative_super_bal += monthly_super_contrib
+        contrib_fee = 0
+    insurance_premium = 0
+    indirect_costs = (0.002 / 12) * cumulative_super_bal # Indirect unforeseen costs 0.2% p.a.
+    super_fees = admin_fee + investment_fee + contrib_fee + insurance_premium + indirect_costs
     
-    return monthly_super_contrib, cumulative_super_bal
+    if model_phase == 1:
+        # Returns - cumulative value of superannuation
+        if cumulative_super_bal == 0:
+            cumulative_super_val += monthly_super_contrib + initial_super_bal
+            print('in bal = 0 loop, cum_val: ', cumulative_super_val)
+        else:
+            cumulative_super_val = (cumulative_super_val + monthly_super_contrib - super_fees) * (1 + (0.06 / 12)) # 8% return p.a.
+            # cumulative_super_val + monthly_super_contrib
+            
+        # Cumulative Contributions to super
+        if cumulative_super_bal == 0:
+            # Adds initial balance to monthly super balance
+            cumulative_super_bal += monthly_super_contrib + initial_super_bal
+        else:
+            # if initial balance is added, just add new contributions
+            cumulative_super_bal += monthly_super_contrib - super_fees
+        
+            
+        return monthly_super_contrib, cumulative_super_bal, super_fees, cumulative_super_val
+
+
+    # Drawdown for Phase 2
+    if model_phase == 2:
+        retirement_income = 100000
+        drawdown_amt = retirement_income / 12
+        monthly_super_contrib = 0
+        cumulative_super_val = (cumulative_super_val - drawdown_amt - super_fees) * (1 + (0.06 / 12))
+        if cumulative_super_val < 0:
+            # Superannuation has been depleted
+            drawdown_amt = 0
+            cumulative_super_val = 0
+            super_fees = 0
+            
+        return monthly_super_contrib, cumulative_super_bal, super_fees, cumulative_super_val, drawdown_amt
 
 
 def expenses_model(model_phase):
     
     if model_phase == 1:
-        return 2000
+        expenses = 1000*np.random.uniform(0.75, 1.25)
+        return expenses
     if model_phase == 2:
-        return 1000
+        expenses = 500*np.random.uniform(0.75, 1.25)
+        return expenses
 
 
 def loan_model(principal, interest_rate, annual_payments, years, period, pmt, begin_balance, end_balance):
@@ -119,7 +157,7 @@ def phase_model(start_date, start_annual_income):
     principal = 350000
     interest_rate = 0.04
     annual_payments = 12
-    years = 15
+    years = 25
     period = 1
     pmt = -round(np.pmt(interest_rate/annual_payments, years*annual_payments, principal), 2)
     begin_balance = principal
@@ -129,7 +167,7 @@ def phase_model(start_date, start_annual_income):
     initial_super_bal = 35000
     monthly_super_contrib = 0
     cumulative_super_bal = 0
-    
+    cumulative_super_val = 0
     
     # PHASE 1
     temp_dt_p1 = start_date # init
@@ -142,7 +180,7 @@ def phase_model(start_date, start_annual_income):
         monthly_income_tax = tax_model(start_annual_income)
         
         # SUPERANNUATION
-        monthly_super_contrib, cumulative_super_bal = super_model(start_annual_income, monthly_super_contrib, initial_super_bal, cumulative_super_bal)
+        monthly_super_contrib, cumulative_super_bal, super_fees, cumulative_super_val = super_model(model_phase, start_annual_income, monthly_super_contrib, initial_super_bal, cumulative_super_bal, cumulative_super_val)
         
         # LIVING EXPENSES
         monthly_expenses = expenses_model(model_phase)
@@ -156,7 +194,8 @@ def phase_model(start_date, start_annual_income):
                            ('Earned_Income_Tax', monthly_income_tax),
                            ('Employer_Super_Contrib', monthly_super_contrib),
                            ('Super_Balance_Contrib', cumulative_super_bal),
-                           ('Super_Balance_Value', 0),
+                           ('Super_Fees', super_fees),
+                           ('Super_Balance_Value', cumulative_super_val),
                            ('Expenses', monthly_expenses),
                            ('Loan_Period', period),
                            ('Loan_Begin_Balance', begin_balance),
@@ -172,9 +211,15 @@ def phase_model(start_date, start_annual_income):
         
         monthly_expenses = expenses_model(model_phase)
         
+        monthly_super_contrib, cumulative_super_bal, super_fees, cumulative_super_val, drawdown_amt = super_model(model_phase, start_annual_income, monthly_super_contrib, initial_super_bal, cumulative_super_bal, cumulative_super_val)
+        
         yield OrderedDict([('Date', temp_dt_p2), 
-                                ('Model_Phase', model_phase),
-                                ('Expenses', monthly_expenses)])
+                           ('Model_Phase', model_phase),
+                           ('Expenses', monthly_expenses),
+                           ('Super_Fees', super_fees),
+                           ('Super_Balance_Value', cumulative_super_val),
+                           ('Super_Drawdown', drawdown_amt)
+                           ])
         
         temp_dt_p2 += relativedelta(months=1)
 
@@ -184,7 +229,7 @@ def agg_function(df):
     # print(df.columns)
     # Replace NaNs with 0s
     df.fillna(0, inplace=True)
-    df['Disposable_Income'] = df['Earned_Income'] - df['Expenses'] - df['Loan_Payment']
+    df['Disposable_Income'] = df['Earned_Income'] + df['Super_Drawdown'] - df['Expenses'] - df['Loan_Payment']
 
     return df
 
@@ -195,10 +240,20 @@ def main():
     df = agg_function(df)
 
     # FOR DEBUGGING
-    print(df.head())
-    print(df.tail())
-    # plt.scatter(x=df['Date'], y=df['Disposable_Income'])
-    # plt.show()
+    print(df.head(12))
+    print(df.tail(12))
+    
+    plt.subplot(211)
+    plt.plot(df['Date'], df['Super_Balance_Value'])
+
+    plt.ylabel('$')
+    plt.title('Super Balance')
+    plt.subplot(212)
+    plt.plot(df['Date'], df['Disposable_Income'])
+    plt.xlabel('Date')
+    plt.ylabel('$')
+    plt.title('Disposable Income')
+    plt.show()
 
 
 if __name__ == '__main__':
