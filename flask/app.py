8ebc31324f967f466adb7c0e0df08de52268d68d
Tyler Bikaun <15507130@student.curtin.edu.au>
2019-11-25 07:53:37 +0800

    basic flask and jquery functionality for SPA.

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    title = 'Our Website'
+    return render_template('test.html', title=title)
+
+@app.route('/data')
+def data():
+    my_data = {
+        'title': 'Tyler',
+        'badges': ['one', 'two', 'three']
+    }
+    
+    return jsonify(my_data)
